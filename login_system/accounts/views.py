fd60d78b8317e386a757b30944ae181ae653fb69
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-06-22 22:50:20 +0600

    fea: register form create and use by defualt form in django.
    1.created before the register template now upadate this template by make register form, use by defualt form in django.

--- login_system/accounts/views.py
+++ login_system/accounts/views.py
@@ -3,11 +3,19 @@ from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
 
+
 def registerPage(request):
-    context={}
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form':form}
     return render(request, 'accounts/register.html',context)
 def loginPage(request):
-    context={}
+    context = {}
     return render(request, 'accounts/login.html',context)
 def homePage(request):
     return render(request, 'accounts/home.html')
