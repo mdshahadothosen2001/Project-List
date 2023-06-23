f16643f265779285055ff763b9a8dc8338d50a81
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-06-23 09:39:19 +0600

    fea: logout permanently.
    1.logout permanently after logout action, and not need to register or loging after login.

--- login_system/accounts/views.py
+++ login_system/accounts/views.py
@@ -5,44 +5,47 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required 
 
 from .templates.accounts.forms import UserForm
 
 
 def registerPage(request):
-    form = UserForm()
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = UserForm()
+        if request.method == 'POST':
+            form = UserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request,'Account Created for ' + user)
+                return redirect('login')
 
-    if request.method == 'POST':
-        form = UserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            
-            user = form.cleaned_data.get('username')
-            messages.success(request,'Account Created for ' + user)
-            return redirect('login')
-
-    context = {'form':form}
-    return render(request, 'accounts/register.html',context)
+        context = {'form':form}
+        return render(request, 'accounts/register.html',context)
 def loginPage(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        user = authenticate(request, username=username, password=password)
-
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             return redirect('home')
         else:
             messages.info(request, 'Username or Password incorrect ! please check this')
             
-    context = {}
-    return render(request, 'accounts/login.html',context)
+        context = {}
+        return render(request, 'accounts/login.html',context)
 def logoutPage(request):
     logout(request)
     return redirect('login')
 
-
+@login_required(login_url='login')
 def homePage(request):
     return render(request, 'accounts/home.html')
 
