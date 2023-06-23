97dafbdaf3dce58a489e0f17c6b65ba319f4ff2d
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-06-23 21:41:05 +0600

    fea: custom user fields.
    1.use abstractuser and include some specific fields.
    
    2.create superuser by phone number

---
+++ custom_user_model/accounts/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .manager import UserManager
+
+class CustomUser(AbstractUser):
+    username = None
+    phone_number = models.CharField(max_length=11, unique=True)
+    father_name = models.CharField(max_length=55)
+    mother_name = models.CharField(max_length=55)
+    brother_name = models.CharField(max_length=55)
+    sister_name = models.CharField(max_length=55)
+    age = models.IntegerField(null=True, blank=True)
+    email = models.EmailField(unique=True)
+    gender = models.CharField(max_length=10)
+    religion = models.CharField(max_length=20)
+    nationality = models.CharField(max_length=55)
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = []
+    objects = UserManager()
