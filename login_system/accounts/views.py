aa58e8745ffec2d0acd08dfdb595b08eea5950f5
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-06-23 09:05:19 +0600

    fea: login authentication and error messages.
    1.check it is correct username and password then login otherwise display message at login page.
    
    2.redirect home page after complated login.

--- login_system/accounts/views.py
+++ login_system/accounts/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+
+from django.contrib.auth import authenticate, login
+
 from .templates.accounts.forms import UserForm
 
 
@@ -21,6 +24,18 @@ def registerPage(request):
     context = {'form':form}
     return render(request, 'accounts/register.html',context)
 def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username or Password incorrect ! please check this')
+            
     context = {}
     return render(request, 'accounts/login.html',context)
 def homePage(request):
