76a887fed4d466db6adcdc7a345bbbc2a69834e0
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-07-01 20:41:48 +0600

    fea: [learning_concepts] make a template view basic using function based concepts.
    1.make a function of basic view for template where using  render, return template as a render function.

--- learning_concepts/develop/urls.py
+++ learning_concepts/develop/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from .views import home_page
 urlpatterns=[
-    path('home/', home_page.as_view(), name='home'),
+    path('home/', home_page, name='home'),
 ]
--- learning_concepts/develop/views.py
+++ learning_concepts/develop/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.views import View
 
-class home_page(View):
-    def get(self, request):
-        return render(request, 'develop/home_page.html')
+def home_page(request):
+    return render(request, 'develop/home_page.html')
