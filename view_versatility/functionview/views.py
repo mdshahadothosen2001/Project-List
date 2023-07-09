840f8d4bfe653400329834a61716fb89c778533e
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-07-09 10:52:12 +0600

    fea: view a template using function based concept.
    1.make a function for display message with template.

--- view_versatility/functionview/views.py
+++ view_versatility/functionview/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home_template_view(request):
+def message_view(request):
     context = "Hello World!"
     return HttpResponse(context)
+
+def home_template_view(request):
+    context = {
+        'message':"Hello World!"
+    }
+    return render(request, 'functionview/home.html', context)
