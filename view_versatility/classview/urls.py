1fc678f2d894903328ed6386eb07f10f3c3e54ca
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-07-10 18:13:53 +0600

    fea: make view based class for template using get method.
    1.make a view for template using get method class based, then return as a render with request and template name and context.

--- view_versatility/classview/urls.py
+++ view_versatility/classview/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import message_display, home_view
+from .views import message_display, home_view, home_with_get_method
 urlpatterns = [
-    path('',message_display.as_view(), name='message'), 
+    path('',message_display.as_view(), name='message'),
+    path('home_get',home_with_get_method.as_view(), name='home_get'),
     path('home',home_view.as_view(), name='home'),
 ]
--- view_versatility/classview/views.py
+++ view_versatility/classview/views.py
@@ -1,11 +1,19 @@
 from django.http import HttpResponse
 from django.views import View
 from django.views.generic import TemplateView
+from django.shortcuts import render
 
 class message_display(View):
     def get(self, request):
         context = 'Hello World!'
         return HttpResponse(context)
+
+class home_with_get_method(View):
+    def get(self, request):
+        context = {
+            'message' : 'Hello World!'
+        }
+        return render(request, 'classview/home.html', context)
 
 class home_view(TemplateView):
     template_name = 'classview/home_view.html'
