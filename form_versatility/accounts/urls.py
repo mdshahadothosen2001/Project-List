aac6d3577e893313cb3970748b0f0f0b1fa4f69e
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-07-13 20:45:02 +0600

    fea: car_list of car model.
    1.make a veiw for display data from car model.

--- form_versatility/accounts/urls.py
+++ form_versatility/accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import car_form
+from .views import car_form, car_list
 urlpatterns = [
     path('car-form/', car_form, name='car_form'),
+    path('cars/', car_list.as_view(), name='cars')
 ]
