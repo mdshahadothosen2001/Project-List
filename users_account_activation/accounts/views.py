094b407f156912fa5630d85d6196da72469d2b35
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-07-28 23:17:25 +0600

    fea: make register and login page.
    1. make form for this model, then make views for register and login and home page, assign urls for this views. then set login required for home page, required for see home page, if login then do not see register and login page just stay home page if need to out required logout.

---
+++ users_account_activation/accounts/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+from django.contrib.auth.decorators import login_required 
+
+def registerPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CustomUserCreationForm()
+        if request.method == 'POST':
+            form = CustomUserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request,'Account Created for ' + str(user))
+                return redirect('login')
+
+        context = {'form':form}
+        return render(request, 'accounts/register.html',context)
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username or Password incorrect ! please check this')
+            
+        context = {}
+        return render(request, 'accounts/login.html',context)
+def logoutPage(request):
+    logout(request)
+    return redirect('login')
+
+@login_required(login_url='login')
+def homePage(request):
+    return render(request, 'accounts/home.html')
+
