507b7dae4b236686cdea7c51d0a463be66a8c323
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-08-01 22:52:32 +0600

    fea: add some specific token fields.
    1. include user first_name, last_name, current_time, current_date, day_status, location. day_status if pm then night otherwise am.

--- jwt_authentication/accounts/api/views.py
+++ jwt_authentication/accounts/api/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from datetime import datetime
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -12,6 +13,19 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         token = super().get_token(user)
 
         token['email'] = user.email
+        token['first_name'] = user.first_name
+        token['last_name'] = user.last_name
+        token['current_time'] = datetime.now().strftime('%Y:%m:%d')
+        current_time = datetime.now().strftime('%I:%M:%p')
+
+        if 'AM' in current_time:
+            token['Day_status'] = 'Day'
+        else:
+            token['Day_status'] = 'Night'
+
+        token['current_date'] = current_time
+        token['location'] = 'Dhaka'
+        
 
         return token
     
