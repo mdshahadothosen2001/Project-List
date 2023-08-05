eec8d9648c87f811c91ece7cd991e199d7bca32f
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-08-05 11:57:56 +0600

    fea: user account activation.
    1. user account activation by otp received through email.

--- jwt_authentication/accounts/views.py
+++ jwt_authentication/accounts/views.py
@@ -4,6 +4,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.exceptions import ValidationError
 from .serializers import UserRegistrationSerializer
 from .otp_send import send_otp_to_email
+from .models import CustomUser
 from .models import OTP
 
 
@@ -34,3 +35,32 @@ class UserRegistrationView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response('succesfull! user is created')
+
+class UserActivation(APIView):
+    """User can activate account by OTP received through email."""
+    
+    def post(self, request, *args, **kwargs):
+        email = request.data.get('email')
+        otp = request.data.get('otp')
+
+        if not email or not otp:
+            raise ValidationError('Please provide both email and OTP.')
+
+        try:
+            otp_obj = OTP.objects.get(email=email, otp=otp)
+            otp_obj.delete()
+
+            user = CustomUser.objects.get(email=email)
+            user.is_active = True
+            user.save()
+
+            return Response('Your account has been activated!')
+
+        except OTP.DoesNotExist:
+            return Response('Invalid OTP, Please double check!')
+
+        except CustomUser.DoesNotExist:
+            return Response('No user found with this email address.')
+
+        except Exception as e:
+            return Response('Something went wrong. Please try again later.')
