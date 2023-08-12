4c88dc97963e7d44a3522ac0661df97185e1c4b9
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-08-12 13:28:04 +0600

    fea: add middleware.
    1.used middleware concept for request and response api, and print for test. here have authentication, security, session, common, and custom middleware

---
+++ signal_middleware/accounts/middlewares.py
@@ -0,0 +1,20 @@
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+class RequestLoggerMiddleware:
+    """used middleware concept for request and response api, and print for test.
+       here have authentication, security, session, common, and custom middleware"""
+    
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        print("RequestLoggerMiddleware: Before view")
+
+        response = self.get_response(request)
+
+        print("RequestLoggerMiddleware: After view")
+
+        return response
