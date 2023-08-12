fb2acbce88cd887dae9dfb115196708ed71270bb
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-08-12 13:03:20 +0600

    fea: add signals for sending email to users.
    1.sending email to users  when user do registration thier account. import signals in apps file directory and make signals function for sending welcome email.

---
+++ signal_middleware/accounts/signals.py
@@ -0,0 +1,16 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.mail import send_mail
+from .models import User
+
+
+@receiver(post_save, sender=User)
+def send_welcome_email(sender, instance, **kwargs):
+    """notify to user through email"""
+
+    if kwargs.get('created', False):
+        subject = 'Welcome to Signal Middleware Project'
+        message = f'Hello {instance.email}, thank you for registering!'
+        from_email = 'admin@example.com'
+        recipient_list = [instance.email]
+        send_mail(subject, message, from_email, recipient_list)
