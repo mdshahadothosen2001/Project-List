5ddf24c2d880884054d1ca731bc0e6bca510b081
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-08-14 18:26:21 +0600

    Fix pre-commit configuration format

--- jwt_authentication/accounts/urls.py
+++ jwt_authentication/accounts/urls.py
@@ -1,29 +1,24 @@
 from django.urls import path
-from .views import UserRegistrationView, UserActivationView, UserPasswordResetView, ForgottenPasswordResetView
+from .views import (
+    UserRegistrationView,
+    UserActivationView,
+    UserPasswordResetView,
+    ForgottenPasswordResetView,
+)
 
 urlpatterns = [
-#localhost:8000/api-auth/register/
+    # localhost:8000/api-auth/register/
     path(
-        route="register/",
-        view=UserRegistrationView.as_view(),
-        name='user_registration'
+        route="register/", view=UserRegistrationView.as_view(), name="user_registration"
     ),
-#localhost:8000/api-auth/activate/
-    path(
-        route="activate/",
-        view=UserActivationView.as_view(),
-        name='user_activation'
-    ),
-#localhost:8000/api-auth/reset/
-    path(
-        route="reset/",
-        view=UserPasswordResetView.as_view(),
-        name='change_password'
-    ),
-    #localhost:8000/api-auth/recover/
+    # localhost:8000/api-auth/activate/
+    path(route="activate/", view=UserActivationView.as_view(), name="user_activation"),
+    # localhost:8000/api-auth/reset/
+    path(route="reset/", view=UserPasswordResetView.as_view(), name="change_password"),
+    # localhost:8000/api-auth/recover/
     path(
         route="recover/",
         view=ForgottenPasswordResetView.as_view(),
-        name='forgotten_password'
+        name="forgotten_password",
     ),
 ]
--- jwt_authentication/jwt_authentication/urls.py
+++ jwt_authentication/jwt_authentication/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('accounts.api.urls')),
-    path('api-auth/', include('accounts.urls')),
+    path("admin/", admin.site.urls),
+    path("api/", include("accounts.api.urls")),
+    path("api-auth/", include("accounts.urls")),
 ]
