4f009684446f10e7726db7325ade42f3fbb5f670
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-09-05 20:25:30 +0600

    fea: make view for display product detail.

--- production_order/base/urls.py
+++ production_order/base/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ProductListView
+from .views import ProductListView, ProductDetailView
 
 urlpatterns = [
     # http://127.0.0.1:8000/
-    path(route="", view=ProductListView.as_view(), name="create_order"),
+    path(route="", view=ProductListView.as_view(), name="products"),
+    # http://127.0.0.1:8000/product/<int:pk>/
+    path(route="product/<int:pk>/", view=ProductDetailView.as_view(), name="product"),
 ]
