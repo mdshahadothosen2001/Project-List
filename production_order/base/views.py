e3ddff6d994287c4a04e9fbad6bb3a7d71309418
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-09-05 21:49:35 +0600

    fea: order generate from product list.
    1.make view for generate specific product order.

--- production_order/base/views.py
+++ production_order/base/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.urls import reverse_lazy
 from product.models import Product
+from order.models import Order
 
 
 class ProductListView(ListView):
@@ -11,11 +13,27 @@ class ProductListView(ListView):
     model = Product
     template_name = "product/product_list.html"
     context_object_name = "products"
+    success_url = reverse_lazy("products")
 
 
-class ProductDetailView(LoginRequiredMixin, DetailView):
+class ProductOrderConfirmView(LoginRequiredMixin, DetailView):
     """User can Order from product table"""
 
     model = Product
-    template_name = "product/product_detail.html"
+    template_name = "product/product_order.html"
     context_object_name = "product"
+
+    def get_context_data(self, **kwargs):
+        """Select product for order"""
+
+        context = super().get_context_data(**kwargs)
+
+        if self.request.user.is_authenticated:
+
+            selected_product = context["product"]
+            if selected_product:
+                order = Order.objects.create(user=self.request.user)
+                order.products.add(selected_product)
+                order.save()
+                context["message"] = "order confirmed"
+        return context
