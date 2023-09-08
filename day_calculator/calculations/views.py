bb02221719cdc2b748108faf7db6c5943ee1a150
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-09-08 15:20:58 +0600

    docs: update for include project functions and usages process.

--- day_calculator/calculations/views.py
+++ day_calculator/calculations/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
 from datetime import timedelta, date
 
 
@@ -13,18 +14,30 @@ class DateCalculationView(APIView):
         last_date = request.data.get("last_date")
 
         if first_date and last_date:
-            first_year, first_month, first_day = map(int, first_date.split("-"))
-            last_year, last_month, last_day = map(int, last_date.split("-"))
-            first_date = date(first_year, first_month, first_day)
-            last_date = date(last_year, last_month, last_day)
-            total_days = (last_date - first_date).days + 1
-            fridays = sum(
-                1
-                for d in range(total_days)
-                if (first_date + timedelta(days=d)).weekday() == 4
-            )
-            day_without_friday = total_days - fridays
+            if "-" in first_date and "-" in last_date:
+                if first_date.count("-") == 2 and last_date.count("-") == 2:
+                    first_year, first_month, first_day = map(int, first_date.split("-"))
+                    last_year, last_month, last_day = map(int, last_date.split("-"))
+                    first_date = date(first_year, first_month, first_day)
+                    last_date = date(last_year, last_month, last_day)
 
-            return Response({"total_days": day_without_friday})
+                    if first_date and last_date:
+                        total_days = (last_date - first_date).days + 1
+                        fridays = sum(
+                            1
+                            for d in range(total_days)
+                            if (first_date + timedelta(days=d)).weekday() == 4
+                        )
+                        day_without_friday = total_days - fridays
 
-        return Response("Double check please")
+                        return Response({"total_days": day_without_friday})
+
+        return Response(
+            {
+                "message": "Double check please",
+                "request data format": {
+                    "first_date": "2023-08-08",
+                    "last_date": "2023-09-09",
+                },
+            }
+        )
--- name_picker/name_picker/urls.py
+++ name_picker/name_picker/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("names/", include("names.urls")),
+    path("", include("names.urls")),
 ]
