1b131603da2f16063f8f0bca7b67b7490b8b4ae7
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-09-14 19:53:30 +0600

    fea: make multiple user table.
    1. here parent model is user, see all user at user model. and base model is student, student model have user which role is student.

---
+++ multiple_user_model/accounts/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db.models.query import QuerySet
+
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN", "Admin"
+        STUDENT = "STUDENT", "Student"
+        TEACHER = "TEACHER", "Teacher"
+
+    base_role = Role.ADMIN
+
+    role = models.CharField(max_length=55, choices=Role.choices)
+
+    def save(self, *args, **kwarg):
+        if not self.pk:
+            self.role = self.base_role
+            return super().save(*args, **kwarg)
+
+
+class StudentManager(BaseUserManager):
+    def get_queryset(self, *args, **kwargs):
+        result = super().get_queryset(*args, **kwargs)
+        return result.filter(role=User.Role.STUDENT)
+
+
+class Student(User):
+    base_role = User.Role.STUDENT
+
+    student = StudentManager()
+
+    class Meta:
+        proxy = True
