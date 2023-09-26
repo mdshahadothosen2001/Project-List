0b3c17a6050e6fef044bc3a5775f630131dc07ea
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-09-26 22:05:07 +0600

    feat: created task model in sorting_algorithm project.

--- sorting_algorithm/task/admin.py
+++ sorting_algorithm/task/admin.py
@@ -1,1 +1,17 @@
 from django.contrib import admin
+from .models import task
+
+
+class TaskAdmin(admin.ModelAdmin):
+    """custom interface at admin panel"""
+
+    list_display = (
+        "id",
+        "title",
+        "description",
+    )
+    list_display_links = ("title",)
+    search_fields = ("title",)
+
+
+admin.site.register(task, TaskAdmin)
